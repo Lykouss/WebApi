ecabbb92bc2c3291fc2369e8324e3a25ce6c9e5d
Lykous <gamesdavid896@gmail.com>
2025-01-29 11:30:22 -0300

    Quarto Commit

--- Controllers/UsuariosController.cs
+++ Controllers/UsuariosController.cs
@@ -2,6 +2,10 @@ using Exo.WebApi.Models;
 using Exo.WebApi.Repositories;
 using Microsoft.AspNetCore.Mvc;
 using System;
+using Microsoft.IdentityModel.Tokens;
+using System.IdentityModel.Tokens.Jwt;
+using System.Security.Claims;
+using Microsoft.AspNetCore.Authorization;
 
 namespace Exo.WebApi.Controllers
 {
@@ -25,11 +29,42 @@ namespace Exo.WebApi.Controllers
         }
 
         // POST -> /api/usuarios
+        // [HttpPost]
+        // public IActionResult Cadastrar(Usuario usuario)
+        // {
+        //     _usuarioRepository.Cadastrar(usuario);
+        //     return StatusCode(201);
+        // }
+
+        // POST -> /api/usuarios
         [HttpPost]
-        public IActionResult Cadastrar(Usuario usuario)
+        public IActionResult Post(Usuario usuario)
         {
-            _usuarioRepository.Cadastrar(usuario);
-            return StatusCode(201);
+            Usuario usuarioBuscado = _usuarioRepository.Login(usuario.Email, usuario.Senha);
+            if (usuarioBuscado == null)
+            {
+                return NotFound("E-mail ou senha inválidos!");
+            }
+
+            // Se o usuário for encontrado, segue a criação do token.
+            var claims = new[]
+            {
+                new Claim(JwtRegisteredClaimNames.Email, usuarioBuscado.Email),
+                new Claim(JwtRegisteredClaimNames.Jti, usuarioBuscado.Id.ToString()),
+            };
+
+            var key = new SymmetricSecurityKey(System.Text.Encoding.UTF8.GetBytes("exoapi-chaveautenticacao"));
+            var creds = new SigningCredentials(key, SecurityAlgorithms.HmacSha256);
+
+            var token = new JwtSecurityToken(
+                issuer: "exoapi.webapi",
+                audience: "exoapi.webapi",
+                claims: claims,
+                expires: DateTime.Now.AddMinutes(30),
+                signingCredentials: creds
+            );
+
+            return Ok(new { token = new JwtSecurityTokenHandler().WriteToken(token) });
         }
 
         // GET -> /api/usuarios/{id}
@@ -45,6 +80,7 @@ namespace Exo.WebApi.Controllers
         }
 
         // PUT -> /api/usuarios/{id}
+        [Authorize]
         [HttpPut("{id}")]
         public IActionResult Atualizar(int id, Usuario usuario)
         {
@@ -53,6 +89,7 @@ namespace Exo.WebApi.Controllers
         }
 
         // DELETE -> /api/usuarios/{id}
+        [Authorize]
         [HttpDelete("{id}")]
         public IActionResult Deletar(int id)
         {
--- Program.cs
+++ Program.cs
@@ -1,10 +1,33 @@
 using Exo.WebApi.Contexts;
 using Exo.WebApi.Repositories;
+using Microsoft.IdentityModel.Tokens;
 
 var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddScoped<ExoContext, ExoContext>();
 builder.Services.AddControllers();
+
+// Forma de autenticação.
+builder.Services.AddAuthentication(options =>
+{
+    options.DefaultAuthenticateScheme = "JwtBearer";
+    options.DefaultChallengeScheme = "JwtBearer";
+})
+// Parâmetros de validação do token.
+.AddJwtBearer("JwtBearer", options =>
+{
+    options.TokenValidationParameters = new TokenValidationParameters
+    {
+        ValidateIssuer = true, // Valida quem está solicitando.
+        ValidateAudience = true, // Valida quem está recebendo.
+        ValidateLifetime = true, // Define se o tempo de expiração será validado.
+        IssuerSigningKey = new SymmetricSecurityKey(System.Text.Encoding.UTF8.GetBytes("exoapi-chaveautenticacao")), // Chave de autenticação.
+        ClockSkew = TimeSpan.FromMinutes(30), // Valida o tempo de expiração do token.
+        ValidIssuer = "exoapi.webapi", // Nome do emissor.
+        ValidAudience = "exoapi.webapi" // Nome do destinatário.
+    };
+});
+
 builder.Services.AddTransient<ProjetoRepository, ProjetoRepository>();
 builder.Services.AddTransient<UsuarioRepository, UsuarioRepository>();
 
